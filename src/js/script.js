90bcf0174e9b82e1d70b6db5db45e76b1d9b4b6b
FelipeGeazi <felipegeaziartes@gmail.com>
2022-06-06 03:16:57 -0300

    finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
+const arrCarrinho = []
+let count = 1
+
+
+
 function montarDados(listaProdutos) {
     for (let contador = 0; contador < listaProdutos.length; contador++) {
         const product = listaProdutos[contador];
@@ -23,23 +28,115 @@ function montaCard(product, listaProduto) {
 
     const bttComprar = document.createElement("button")
     bttComprar.classList.add("bttComprar")
+    bttComprar.id = product.id
     bttComprar.innerText = "Comprar"
 
-    const valorTotal = document.querySelector("#precoTotal")
-    valorTotal.innerText = somaTotal(listaProduto)
+
+    bttComprar.addEventListener('click', () => {
+        product.idCarrinho = count
+        arrCarrinho.push(product)
+        count++
+        montarCarrinho(arrCarrinho)
+        console.log(arrCarrinho)
+    })
 
     card.append(imagens, texto, bttComprar)
     listaProdutos.append(card)
 
 }
 
+function criaItemCarrinho(item) {
+
+    const itemCarrinho = document.createElement("li")
+    itemCarrinho.classList.add("itemCarrinho--li")
+    itemCarrinho.id = item.id
+
+    const imgCarrinho = document.createElement("img")
+    imgCarrinho.src = item.img
+    imgCarrinho.classList.add("itemCarrinho--img")
+
+    const divTextoCarrinho = document.createElement("div")
+    divTextoCarrinho.classList.add("divTextoCarrinho")
+
+    const nameTitulo = document.createElement("p")
+    nameTitulo.classList.add("itemCarrinho--Produto")
+    nameTitulo.innerText = item.nome
+
+
+    const secaoCar = document.createElement("p")
+    secaoCar.classList.add("itemCarrinho--secao")
+    secaoCar.innerText = item.secao
+
+
+    const valorItem = document.createElement("p")
+    valorItem.classList.add("valorItem")
+    valorItem.innerText = `R$ ${item.preco},00`
+
+    removerProduto = document.createElement("button")
+    removerProduto.classList.add("bttRemover")
+    removerProduto.innerText = "⛔"
+    removerProduto.id = item.id
+        /* console.log(removerProduto.id) */
+
+
+    removerProduto.addEventListener("click", tiraItemCarrinho)
+
+
+    divTextoCarrinho.append(nameTitulo, secaoCar, valorItem, )
+    itemCarrinho.append(imgCarrinho, divTextoCarrinho, removerProduto)
+    return itemCarrinho
+
+}
+
+function tiraItemCarrinho(e) {
+    /* 
+        const indeX = arrCarrinho.filter((element, index, arr) => {
+            e.target = arr[index].id
+        }) */
+
+    const newItem = arrCarrinho.find(element => {
+        return element.id == e.target.id
+    })
+    const index = arrCarrinho.indexOf(newItem)
+
+    arrCarrinho.splice(index, 1)
+
+    montarCarrinho(arrCarrinho)
+
+}
+
+function retiraItem() {
+
+    const listaCarrinho = document.getElementById("listaItensCarrinho")
+    console.log(arrCarrinho)
+}
 
 
 
+function montarCarrinho(arrCarrinho) {
+
+    const listaCarrinho = document.getElementById("listaItensCarrinho")
+    listaCarrinho.innerHTML = ""
+
+    const valorTotal = document.querySelector("#precoTotal")
+    valorTotal.innerText = somaTotal(arrCarrinho)
+
+    const quantidadeTotal = document.querySelector("#quantidadeTotal")
+    quantidadeTotal.innerText = arrCarrinho.length
+
+
+    for (let contador = 0; contador < arrCarrinho.length; contador++) {
+        const item = arrCarrinho[contador]
+        const returnCriaItemCarrinho = criaItemCarrinho(item);
+        listaCarrinho.append(returnCriaItemCarrinho)
+    }
+
+    console.log(listaCarrinho)
+}
 
 function criaImage(product) {
     const image = document.createElement('img')
-    image.classList.add("containerListaProdutos")
+    image.classList.add("containerListaProdutos--img")
     image.src = product.img
     return image
 
@@ -49,58 +146,35 @@ function montaTexto(product) {
     const text = document.createElement('div');
 
     const name = document.createElement('h3');
-    name.classList.add("containerListaProdutos");
+    name.classList.add("containerListaProdutos--h3");
     name.innerText = product.nome;
 
 
     var valor = document.createElement('p');
-    valor.classList.add("containerListaProdutos");
+    /*  valor.classList.add("containerListaProdutos--p"); */
+    valor.classList.add("containerListaProdutos--span")
     valor.innerText = ` R$ ${product.preco},00 `
-        /* const valorNumero = parseInt(valor) */
-
 
     var categoria = document.createElement('span');
-    categoria.classList.add("containerListaProdutos");
+    categoria.classList.add("containerListaProdutos--span");
     categoria.innerText = ` ${product.secao}`
-
 
     const divComponentes = montaComponentes(product.componentes)
 
-
-
     text.append(name, categoria, divComponentes, valor)
     return text
-
 }
-
-
-/* function montaComponentes(lista) {
-    let ol = document.createElement("ol")
-    for (let i = 0; i < lista.length; i++) {
-        let item = document.createElement("p")
-        item.innerText = lista[i]
-        console.log(lista[i])
-        ol.append(item)
-    }
-    return ol
-
-
-}
- */
-//  mesma coisa que em cima 👇👇👇👇👇👇
 
 function montaComponentes(lista) {
     let ol = document.createElement("ol")
     lista.forEach((element, index) => {
         let item = document.createElement("p")
-        item.classList.add("componentesProduto")
+        item.classList.add("containerListaProdutos--p")
         item.innerText = `${index + 1}. ${element}`
         ol.append(item)
     });
     return ol
 }
-
-
 
 
 function somaTotal(listaProdutos) {
@@ -109,10 +183,8 @@ function somaTotal(listaProdutos) {
         result += listaProdutos[i].preco
     }
 
-    return `${result},00`
+    return `R$${result},00`
 }
-
-
 
 
 // filtrando por secao com filter
@@ -127,52 +199,14 @@ function filtrarPorHotifruti() {
 
 }
 
-/* function filtrarPanificacao() {
-    const listaProdutos = document.querySelector("ul")
-    listaProdutos.innerHTML = ""
-
-    const listaPani = produtos.filter((produto) => {
-        return produto.secao === 'Panificadora'
-    })
-    montarDados(listaPani)
-
-}
-
-function filtrarLaticio() {
-    const listaProdutos = document.querySelector("ul")
-    listaProdutos.innerHTML = ""
-
-    const listaLaticinio = produtos.filter((produto) => {
-        return produto.secao === 'Laticínio'
-    })
-    montarDados(listaLaticinio)
-
-} */
-
-//filtrarPorHotifruti()
-//adicionando o event listener de clique e executando a função filter
-const buttonHortFrut = document.querySelector('#botaoFruts')
-buttonHortFrut.addEventListener('click', filtrarPorHotifruti)
-
-/* const buttonPani = document.querySelector('#botaoPanificadora')
-buttonHortFrut.addEventListener('click', filtrarPanificacao)
-
-const buttonLaticinio = document.querySelector('#botaoLaticinios')
-buttonHortFrut.addEventListener('click', filtrarLaticio) */
 
 
 
 function filtrarTodos(event) {
     const listaTodos = document.querySelector("ul")
     listaTodos.innerHTML = ""
-
     montarDados(produtos)
-
-
 }
-
-const buttonTodos = document.querySelector('.estiloGeralBotoes--mostrarTodos')
-buttonTodos.addEventListener('click', filtrarTodos)
 
 
 // barra de pesquisa
@@ -183,8 +217,6 @@ function buscarNome() {
     const textoPesquisa = document.querySelector(".campoBuscaPorNome").value
     let procura = textoPesquisa.toLowerCase()
 
-
-
     const produtoPesquisado = produtos.filter((produto) =>
         produto.nome.toLowerCase().includes(procura.trim()) ||
         produto.categoria.toLowerCase().includes(procura.trim()) ||
@@ -193,10 +225,17 @@ function buscarNome() {
     montarDados(produtoPesquisado)
 }
 
+// filtros
+
+const buttonHortFrut = document.querySelector('#botaoFruts')
+buttonHortFrut.addEventListener('click', filtrarPorHotifruti)
+
+const buttonTodos = document.querySelector('.estiloGeralBotoes--mostrarTodos')
+buttonTodos.addEventListener('click', filtrarTodos)
+
 
 const buttonPesquisar = document.querySelector(".estiloGeralBotoes--botaoBuscaPorNome")
 buttonPesquisar.addEventListener('click', buscarNome)
-
 
 const pesquisaInput = document.querySelector(".campoBuscaPorNome")
 pesquisaInput.addEventListener('keypress', buscarNome)
